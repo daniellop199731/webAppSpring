a80737463496086c7d3ea484271837fcf7bafc38
Daniel Lopez <daniellop199731@gmail.com>
2025-04-07 16:14:50 -0500

    first commit

---
+++ src/main/java/com/example/webapp/webappspring/WebappspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.webapp.webappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webapp/webappspring/WebappspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webapp.webappspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
