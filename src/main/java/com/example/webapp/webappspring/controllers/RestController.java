157725f81237aea56b63c3073bb7b3ca80cc4680
Daniel Lopez <daniellop199731@gmail.com>
2025-04-08 10:37:49 -0500

    parte 1: Adicion de nombre especifico a controladores

--- src/main/java/com/example/webapp/webappspring/controllers/RestController.java
+++ src/main/java/com/example/webapp/webappspring/controllers/RestController.java
@@ -4,11 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.web.bind.annotation.RestController
+@RequestMapping("/api")
 public class RestController {
 
-    @GetMapping("/details2")
+    @GetMapping("/details")
     public Map<String, Object> details(){
         Map<String, Object> mpJson = new HashMap<>();
         
--- src/main/java/com/example/webapp/webappspring/controllers/UserController.java
+++ src/main/java/com/example/webapp/webappspring/controllers/UserController.java
@@ -3,8 +3,10 @@ package com.example.webapp.webappspring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/home")
 public class UserController {
 
     @GetMapping("/details")
