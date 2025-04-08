5c882131f8b74ef60afee8529ef19f2b98e8d4ec
Daniel Lopez <daniellop199731@gmail.com>
2025-04-08 11:41:31 -0500

    Parte 1: Mostrando lista de objetos en JSON

--- src/main/java/com/example/webapp/webappspring/controllers/RestController.java
+++ src/main/java/com/example/webapp/webappspring/controllers/RestController.java
@@ -1,6 +1,9 @@
 package com.example.webapp.webappspring.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,12 +44,30 @@ public class RestController {
     @GetMapping("/detailsWithUserDto")
     public UserDto detailsWithUserDto() {      
         UserDto userDto = new UserDto();  
-        User user = new User("Daniel", "Lopez");        
+        User user = new User("Daniel", "Lopez");
         userDto.setTittle("Hola Mundo Spring boot");
         userDto.setUser(user);
 
         return userDto;
     }
+
+    @GetMapping("/listOfUsers")
+    public List<User> listOfUsers() {
+        User user = new User("Daniel", "Lopez");
+        User user2 = new User("Andres", "Guzman");
+        User user3 = new User("Camila", "Montoya");
+
+        List<User> users = new ArrayList<>();
+        users.add(user);
+        users.add(user2);
+        users.add(user3);
+
+        //Otra forma de llenar una List
+        List<User> users2 = Arrays.asList(user, user2, user3);
+
+        return users;
+    }
+    
     
     
 }
