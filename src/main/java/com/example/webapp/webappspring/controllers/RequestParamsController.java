7efcb32776965cf486d0c1c21993aee5ebbcf94c
Daniel Lopez <daniellop199731@gmail.com>
2025-04-08 20:55:48 -0500

    Parte 1: Uso de HttpServletRequest

--- src/main/java/com/example/webapp/webappspring/controllers/RequestParamsController.java
+++ src/main/java/com/example/webapp/webappspring/controllers/RequestParamsController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.webapp.webappspring.models.dto.ParamDto;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +25,19 @@ public class RequestParamsController {
 
         return param;
     }
+
+    @GetMapping("/request")
+    public ParamDto reques(HttpServletRequest request) {
+        ParamDto param = new ParamDto();
+        try{
+            param.setCode(Integer.parseInt(request.getParameter("code")));
+        } catch (NumberFormatException e){
+            param.setCode(0);
+        }
+        param.setMessage(request.getParameter("message"));
+        return param;
+    }
+    
     
 
 }
