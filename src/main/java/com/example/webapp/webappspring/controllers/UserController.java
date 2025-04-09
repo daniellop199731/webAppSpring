3afba2c8b645b3323b386606d958743fc367965f
Daniel Lopez <daniellop199731@gmail.com>
2025-04-08 20:28:40 -0500

    Parte 1: Uso de @ModelAttribute
    Para tener datos en comun en un mismo controlador

--- src/main/java/com/example/webapp/webappspring/controllers/UserController.java
+++ src/main/java/com/example/webapp/webappspring/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -44,13 +45,28 @@ public class UserController {
         user2.setUserEmail("correo2@correo.com");
         User user3 = new User("Camila", "Montoya");
         user3.setUserEmail("correo3@correo.com");
-        List<User> users = Arrays.asList(user, user2, user3);
-        
+
+        List<User> users = Arrays.asList(user, user2, user3);        
 
         model.addAttribute("users", users);
 
         model.addAttribute("tittle", "Lista de usuarios");
         return "detailsListOfUsers";
+    }
+
+    @ModelAttribute("users")
+    public List<User> usersModel(){
+        User user = new User("Daniel", "Lopez");
+        user.setUserEmail("correo1@correo.com");
+        User user2 = new User("Andres", "Guzman");
+        user2.setUserEmail("correo2@correo.com");
+        User user3 = new User("Camila", "Montoya");
+        user3.setUserEmail("correo3@correo.com");
+
+        List<User> users = Arrays.asList(user, user2, user3); 
+        
+        return users;
+
     }
     
     
